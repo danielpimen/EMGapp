20c48d01a043c171d661a1378520fea73518126e
Daniel P <danielpimen92@gmail.com>
2018-02-24 03:50:05 -0500

    working for now

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -21,38 +21,39 @@ var dataRef = new Firebase(url);
 $(document).ready(function() {
 
     $('#add-word').on('click', function(event) {
-        event.preventDefault();
         var projectNumber = $('#project-input').val().trim();
         var requestedServices = $('#service-input').val().trim();
         var requestedDate = $('#date-input').val().trim();
         var userComment = $('#comment-input').val().trim();
         console.log(projectNumber, requestedServices, requestedDate, userComment);
 
+            keyHolder = dataRef.push({
+                projectNumber: projectNumber,
+                requestedServices: requestedServices,
+                requestedDate: requestedDate, 
+                userComment: userComment,
+            });
+        });
+
         dataRef.on("child_added", function input(childSnapshot) {
 
-        $('.emgSchedule').append("<tr class='table-row' id=" + "'" + childSnapshot.key() + "'" + ">" +
-            "<td class='col-xs-3 text-center'>" + childSnapshot.val().projectNumber +
-            "</td>" +
-            "<td class='col-xs-2'>" + childSnapshot.val().requestedServices +
-            "</td>" +
-            "<td class='col-xs-2'>" + childSnapshot.val().requestedDate +
-            "</td>" +
-            "<td class='col-xs-2'>" + childSnapshot.val().userComment +
-            "</td>" +
-            "<td class='col-xs-1'>" + "<input type='submit' value='remove request' class='remove-request btn btn-primary btn-sm'>" + "</td>" +
-            "</tr>");
-        input();
-        keyHolder = dataRef.push({
-            projectNumber: projectNumber,
-            requestedServices: requestedServices,
-            requestedDate: requestedDate, // 2:22 in my example
-            userComment: userComment,
+
+            $('.emgSchedule').append("<tr class='table-row' id=" + "'" + childSnapshot.key() + "'" + ">" +
+                "<td class='col-xs-3 text-center'>" + childSnapshot.val().projectNumber +
+                "</td>" +
+                "<td class='col-xs-2'>" + childSnapshot.val().requestedServices +
+                "</td>" +
+                "<td class='col-xs-2'>" + childSnapshot.val().requestedDate +
+                "</td>" +
+                "<td class='col-xs-2'>" + childSnapshot.val().userComment +
+                "</td>" +
+                "<td class='col-xs-1'>" + "<input type='submit' value='remove request' class='remove-request btn btn-primary btn-sm'>" + "</td>" +
+                "</tr>");
+
         });
     });
-});
     $("body").on("click", ".remove-request", function() {
         $(this).closest('tr').remove();
         getKey = $(this).parent().parent().attr('id');
         dataRef.child(getKey).remove();
     });
-});
