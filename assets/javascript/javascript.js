a102e546d86d0f653c4ffd2f29f0880685cc8429
Daniel P <danielpimen92@gmail.com>
2018-02-24 03:09:48 -0500

    needs some work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,7 @@
 /*
 1) User inputs request
 2) Request is placed into table and database
-3) 
+3)  
 
 */
 var config = {
@@ -18,26 +18,41 @@ var url = "https://quantum-boulder-196206.firebaseio.com";
 var dataRef = new Firebase(url);
 
 
-$('#add-word').on('click', function(event) {
-    event.preventDefault();
-    var projectNumber = $('#project-input').val().trim();
-    var requestedServices = $('#service-input').val().trim();
-    var requestedDate = $('#date-input').val().trim();
-    var userComment = $('#comment-input').val().trim();
-    console.log(projectNumber, requestedServices, requestedDate, userComment);
-    $('.emgSchedule').append("<tr class='table-row' id=" + "'" + "'" + ">" +
-        "<td class='col-xs-3 text-center'>" + projectNumber +
-        "</td>" +
-        "<td class='col-xs-2'>" + requestedServices +
-        "</td>" +
-        "<td class='col-xs-2'>" + requestedDate +
-        "</td>" +
-        "<td class='col-xs-2'>" + userComment +
-        "</td>" +
-        "<td class='col-xs-1'>" + "<input type='submit' value='remove request' class='remove-request btn btn-primary btn-sm'>" + "</td>" +
-        "</tr>");
-});
+$(document).ready(function() {
 
-$("body").on("click", ".remove-request", function() {
-    $(this).closest('tr').remove();
+    $('#add-word').on('click', function(event) {
+        event.preventDefault();
+        var projectNumber = $('#project-input').val().trim();
+        var requestedServices = $('#service-input').val().trim();
+        var requestedDate = $('#date-input').val().trim();
+        var userComment = $('#comment-input').val().trim();
+        console.log(projectNumber, requestedServices, requestedDate, userComment);
+
+        dataRef.on("child_added", function input(childSnapshot) {
+
+        $('.emgSchedule').append("<tr class='table-row' id=" + "'" + childSnapshot.key() + "'" + ">" +
+            "<td class='col-xs-3 text-center'>" + childSnapshot.val().projectNumber +
+            "</td>" +
+            "<td class='col-xs-2'>" + childSnapshot.val().requestedServices +
+            "</td>" +
+            "<td class='col-xs-2'>" + childSnapshot.val().requestedDate +
+            "</td>" +
+            "<td class='col-xs-2'>" + childSnapshot.val().userComment +
+            "</td>" +
+            "<td class='col-xs-1'>" + "<input type='submit' value='remove request' class='remove-request btn btn-primary btn-sm'>" + "</td>" +
+            "</tr>");
+        input();
+        keyHolder = dataRef.push({
+            projectNumber: projectNumber,
+            requestedServices: requestedServices,
+            requestedDate: requestedDate, // 2:22 in my example
+            userComment: userComment,
+        });
+    });
+});
+    $("body").on("click", ".remove-request", function() {
+        $(this).closest('tr').remove();
+        getKey = $(this).parent().parent().attr('id');
+        dataRef.child(getKey).remove();
+    });
 });
